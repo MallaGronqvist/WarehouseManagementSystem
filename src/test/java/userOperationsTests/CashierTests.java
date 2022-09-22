ae17b0aaa18b66dfe5b69b980a327790a8279e93
Malla <mallagronqvist@gmail.com>
2022-09-22 10:42:37 +0200

    Did some refactoring.

--- src/test/java/userOperationsTests/CashierTests.java
+++ src/test/java/userOperationsTests/CashierTests.java
@@ -15,49 +15,34 @@ import utils.Observer;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CashierTests {
-
     ProductPool productPool;
     TransactionPool transactionPool;
-
     OrderPool orderPool;
-
     Transaction transaction;
+    List<InventoryDataItem> products;
+    List<InventoryDataItem> transactions;
+    List<InventoryDataItem> orders;
+    Observer observer;
 
-    // static pools mix it up!!
     @BeforeEach
     void setUp(){
-        List<InventoryDataItem> products = new ArrayList<>();
-        products.add(new Product(46, "Samsung Galaxy", 13, "A11"));
-        products.add(new Product(13, "IPhone 13", 10, "A11"));
-        products.add(new Product(12, "IPhone Original", 1, "A11"));
-        productPool = new ProductPool(products);
+        products = new ArrayList<>();
+        transactions = new ArrayList<>();
+        orders = new ArrayList<>();
 
-        List<InventoryDataItem> transactions = new ArrayList<>();
-        transaction = new Transaction(
-                TransactionType.REMOVAL, "9999",
-                ProductPool.getProductById(46), 1);
-        transactions.add(transaction);
-        transactionPool = new TransactionPool(transactions);
-
-        List<InventoryDataItem> orders = new ArrayList<>();
-        orders.add(new Order(ProductPool.getProductById(13), 50));
-        orderPool = new OrderPool(orders);
-
-        Observer observer = data -> {};
-
-        orderPool.registerObserver(observer);
-
-        transactionPool.registerObserver(observer);
+        observer = data -> {};
     }
 
     @Test
     public void removeItemTest(){
-        Product product = ProductPool.getProductById(13);
+        products.add(new Product(13, "IPhone 13", 10, "A11"));
+        productPool = new ProductPool(products);
+        productPool.registerObserver(observer);
 
+        Product product = ProductPool.getProductById(13);
         product.removeItems(1);
 
         assertEquals(product.getQuantity(), 9);
@@ -65,6 +50,9 @@ public class CashierTests {
 
     @Test
     public void removeLastItemTest(){
+        products.add(new Product(12, "IPhone Original", 1, "A11"));
+        productPool = new ProductPool(products);
+        productPool.registerObserver(observer);
         Product product = ProductPool.getProductById(12);
 
         assertThrows(IllegalArgumentException.class, () -> product.removeItems(1));
@@ -72,48 +60,80 @@ public class CashierTests {
 
     @Test
     public void getTransactionByReceiptTest(){
+        products.add(new Product(46, "Samsung Galaxy", 13, "A11"));
+        productPool = new ProductPool(products);
+        productPool.registerObserver(observer);
+
+        transaction = new Transaction(
+                TransactionType.REMOVAL, "9999",
+                ProductPool.getProductById(46), 1);
+        transactions.add(transaction);
+        transactionPool = new TransactionPool(transactions);
+        transactionPool.registerObserver(observer);
 
         Transaction testTransaction = TransactionPool.getTransactionByReceipt("9999");
-        assertEquals(transaction.getReceiptNumber(), testTransaction.getReceiptNumber());
+
+        assertEquals(transaction.getProduct(), testTransaction.getProduct());
+        assertEquals(transaction.getQuantity(), testTransaction.getQuantity());
+        assertEquals(transaction.getId(), testTransaction.getId());
+        assertEquals(transaction.getType(), testTransaction.getType());
     }
 
     @Test
     public void getTransactionByInvalidReceiptTest(){
+        products.add(new Product(46, "Samsung Galaxy", 13, "A11"));
+        productPool = new ProductPool(products);
+        productPool.registerObserver(observer);
 
-        assertThrows(NullPointerException.class, () -> TransactionPool.getTransactionByReceipt("0"));
+        transaction = new Transaction(
+                TransactionType.REMOVAL, "9999",
+                ProductPool.getProductById(46), 1);
+        transactions.add(transaction);
+        transactionPool = new TransactionPool(transactions);
+        transactionPool.registerObserver(observer);
 
+        assertThrows(NullPointerException.class,
+                () -> TransactionPool.getTransactionByReceipt("0"));
     }
-/*
-    @Test
-    public void placeOrderTest(){
-        Product product = productPool.getProductById(13);
-        int quantity = 100;
 
-        Order testOrder = new Order(product, quantity);
-
-        orderPool.addNeWOrder(testOrder);
-
- //       transactionPool.addNewTransaction(new Transaction(TransactionType.ORDERED, "-", product, quantity));
-
-        assertEquals(testOrder, orderPool.getOrderById(123));
-    }
-*/
     @Test
     public void returnItemUpdatedQuantityTest(){
+        products.add(new Product(1, "Another test product", 1, "A1"));
+        productPool = new ProductPool(products);
+        productPool.registerObserver(observer);
+        transaction = new Transaction(
+                TransactionType.REMOVAL, "9999",
+                ProductPool.getProductById(1), 1);
 
         Product product = transaction.getProduct();
         int quantityBefore = product.getQuantity();
         int addedQuantity = transaction.getQuantity();
         int totalQuantity = quantityBefore + addedQuantity;
+
         product.addItems(transaction.getQuantity());
 
         assertEquals(product.getQuantity(), totalQuantity);
     }
+
+    @Test
+    public void placeOrderCheckIfProductAlreadyOrderedTest(){
+        int quantity = 100;
+        Product product = new Product(2, "Another test product", 1, "A1");
+        Order order = new Order(product, quantity);
+        orders.add(order);
+        orderPool = new OrderPool(orders);
+        orderPool.registerObserver(observer);
+
+        assertTrue(OrderPool.isAlreadyOrdered(product));
+    }
+
+
+
     /*
     @Test
     public void returnItemDeleteTransaction(){
 
-
+//       transactionPool.addNewTransaction(new Transaction(TransactionType.ORDERED, "-", product, quantity));
         transactionPool.removeTransaction(transaction);
 
         assertThrows(NullPointerException.class,
